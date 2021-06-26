3842fbd0e5ed8996edad1245366044e63eea995a
秦娜敏 <787904368@qq.com>
2021-06-26 15:01:53 +0800

    皮带煤量 动图

--- src/views/app_belt_coal_quantity/beltCoalQuantity.vue
+++ src/views/app_belt_coal_quantity/beltCoalQuantity.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>beltCoalQuantity</div>
+  <div class="box">
+    <img src="@/assets/img/conveyor.gif" alt="图片" align="middle">
+  </div>
 </template>
 
 <script>
-export default {
-  name: "beltCoalQuantity"
-}
+    export default {
+        name: "beltCoalQuantity"
+    }
 </script>
-
 <style scoped>
-
+    /* .box {
+        width: 600px;
+        height: 600px;
+        border: 1px solid red;
+        /* background: require("@/assets/img/scenery.gif"); 
+    
+    background: require('@/assets/img/100.png');
+}
+*/
 </style>
